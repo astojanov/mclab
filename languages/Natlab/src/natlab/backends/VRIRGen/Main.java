ac6a6a430c365495d86d539b289ffbc318ff5b75
sameerjagdale <sameerjagdale90@yahoo.co.in>
2013-09-05 16:35:06 -0400

    Package and Classes for VRIR generator created. Classes : Main.java , VrirXmlGen.java

---
+++ languages/Natlab/src/natlab/backends/VRIRGen/Main.java
@@ -0,0 +1,10 @@
+package natlab.backends.VRIRGen;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ languages/Natlab/src/natlab/backends/VRIRGen/VrirXmlGen.java
@@ -0,0 +1,15 @@
+package natlab.backends.VRIRGen;
+
+import ast.ASTNode;
+import nodecases.natlab.NatlabAbstractNodeCaseHandler;
+
+public class VrirXmlGen extends NatlabAbstractNodeCaseHandler {
+
+	@Override
+	public void caseASTNode(ASTNode node) {
+		// TODO Auto-generated method stub
+
+	}
+	
+
+}
