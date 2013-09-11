22ed5ed91ad854bd18be3fdf2440be4ea8474e9f
sameerjagdale <sameerjagdale90@yahoo.co.in>
2013-09-11 15:33:22 -0400

    Symbol and SymbolTable classes added

---
+++ languages/Natlab/src/natlab/backends/VRIRGen/Symbol.java
@@ -0,0 +1,7 @@
+package natlab.backends.VRIRGen;
+
+public class Symbol {
+	VType vtype;
+	String name;
+	int id;
+}
--- languages/Natlab/src/natlab/backends/VRIRGen/SymbolTable.java
+++ languages/Natlab/src/natlab/backends/VRIRGen/SymbolTable.java
@@ -1,5 +1,20 @@
 package natlab.backends.VRIRGen;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SymbolTable {
-	
+	private Map<String, Symbol> symbolMap;
+
+	public SymbolTable() {
+		symbolMap = new HashMap<String, Symbol>();
+	}
+
+	public Symbol getSymbol(String name) {
+		return symbolMap.get(name);
+	}
+
+	public void putSymbol(String name, Symbol sym) {
+		symbolMap.put(name, sym);
+	}
 }
