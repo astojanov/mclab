5cbc8d59e6bbdd07dba2ea0220a0768950a50d51
Vincent Foley <vfoley@gmail.com>
2014-01-29 17:53:53 -0500

    Started working on JavaScript backend; writing AST, pretty printer and pretty printing library in tandem.

---
+++ languages/Natlab/src/natlab/backends/javascript/pretty/PrettyLine.java
@@ -0,0 +1,20 @@
+package natlab.backends.javascript.pretty;
+
+public class PrettyLine extends PrettyBase {
+	private static PrettyLine singleton = null;
+
+	public static PrettyLine getInstance() {
+		if (singleton == null) {
+			singleton = new PrettyLine();
+			return singleton;
+		}
+		return singleton;
+	}
+	
+	private PrettyLine() {
+	}
+	
+	public String show() {
+		return 	String.format("%n");
+	}
+}
