3812a11133bccdf65ff71791af6c3af4afba47ee
Vincent Foley <vfoley@gmail.com>
2014-02-03 16:44:33 -0500

    Simplified class hierarchy for pretty printing.
    - Updated design notes
    - Replaced recursive PrettyUtils.display() method with an iterative
      version.

--- languages/Natlab/src/natlab/backends/javascript/pretty/PrettyUtils.java
+++ languages/Natlab/src/natlab/backends/javascript/pretty/PrettyUtils.java
@@ -4,53 +4,50 @@ import java.util.Stack;
 
 public class PrettyUtils {
     private static int INDENT = 4;
-    
+
     public static String indent(int level) {
         StringBuffer sb = new StringBuffer();
         for (int i = 0; i < INDENT*level; ++i)
             sb.append(' ');
         return sb.toString();
     }
-        
+
     public static String display(PrettyBase root) {
+        int currCol = 0;
+        StringBuffer sb = new StringBuffer();
         Stack<Pair> worklist = new Stack<>();
         worklist.push(new Pair(root, 0));
-        return display(worklist, new StringBuffer(), 0);
-    }
-    
+        
+        while (!worklist.isEmpty()) {
+            Pair pair = worklist.pop();
+            PrettyBase node = pair.prettyNode;
+            int currLevel = pair.level;
 
-    private static String display(Stack<Pair> worklist,
-            StringBuffer sb, int currCol) {
-        if (worklist.isEmpty())
-            return sb.toString();
-        
-        Pair pair = worklist.pop();
-        PrettyBase node = pair.prettyNode;
-        int currLevel = pair.level;
-        
-        if (node instanceof PrettyLine) {
-            currCol = 0;
-            sb.append('\n');
-        }
-        else if (node instanceof PrettyText) {
-            String s = pad(currLevel*INDENT - currCol) + node.toString();
-            currCol += s.length();
-            sb.append(s);
-        }
-        else if (node instanceof PrettyConcat) {
-            PrettyConcat pc = (PrettyConcat) node;
-            for (int i = pc.parts.length - 1; i >= 0; --i) {
-                worklist.push(new Pair(pc.parts[i], currLevel));
+            if (node instanceof PrettyLine) {
+                currCol = 0;
+                sb.append('\n');
             }
-        }
-        else if (node instanceof PrettyIndent) {
-            PrettyIndent pi = (PrettyIndent) node;
-            worklist.push(new Pair(pi.child, currLevel+1));
+            else if (node instanceof PrettyText) {
+                String s = pad(currLevel*INDENT - currCol) + node.toString();
+                currCol += s.length();
+                sb.append(s);
+            }
+            else if (node instanceof PrettyConcat) {
+                PrettyConcat pc = (PrettyConcat) node;
+                for (int i = pc.parts.length - 1; i >= 0; --i) {
+                    worklist.push(new Pair(pc.parts[i], currLevel));
+                }
+            }
+            else if (node instanceof PrettyIndent) {
+                PrettyIndent pi = (PrettyIndent) node;
+                worklist.push(new Pair(pi.child, currLevel+1));
+            }  
         }
         
-        return display(worklist, sb, currCol);
+        return sb.toString();
     }
-    
+
+
     private static String pad(int width) {
         StringBuffer sb = new StringBuffer();
         for (int i = 0; i < width; ++i)
