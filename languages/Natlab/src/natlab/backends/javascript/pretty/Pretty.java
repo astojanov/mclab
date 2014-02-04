90a61bdf0249ac971365e9bc0671cca16ea7ed7d
Vincent Foley <vfoley@gmail.com>
2014-02-03 21:36:49 -0500

    Removed useless Pretty.newline() methods.

--- languages/Natlab/src/natlab/backends/javascript/pretty/Pretty.java
+++ languages/Natlab/src/natlab/backends/javascript/pretty/Pretty.java
@@ -2,46 +2,41 @@ package natlab.backends.javascript.pretty;
 
 public class Pretty {
     public static PrettyBase EMPTY = text("");
-    public static PrettyBase NEWLINE = newline();
-    
+    public static PrettyBase NEWLINE = PrettyLine.getInstance();
+
     public static PrettyBase text(String s) {
         return new PrettyText(s);
     }
-    
-    
-    private static PrettyBase newline() {
-        return PrettyLine.getInstance();
-    }
-    
-    
+
+
     public static PrettyBase concat(PrettyBase... parts) {
         return new PrettyConcat(parts);
     }
-    
-    
+
+
     public static PrettyBase indent(PrettyBase node) {
         return new PrettyIndent(node);
     }
-    
-    
+
+
     public static PrettyBase wrapped(PrettyBase opener, PrettyBase closer, PrettyBase node) {
         return concat(opener, node, closer);
     }
 
-    
+
     public static PrettyBase parenthesized(PrettyBase node) {
         return wrapped(text("("), text(")"), node);
     }
-    
-    
+
+
     public static PrettyBase block(PrettyBase node) {
         return wrapped(
                 concat(text("{"), NEWLINE),
                 concat(NEWLINE, text("}")),
-                separateBy(newline(), node));
+                separateBy(NEWLINE, node));
     }
-    
-    
+
+
     public static PrettyBase separateBy(PrettyBase sep, PrettyBase... parts) {
         PrettyBase[] separatedParts = new PrettyBase[Math.max(0, 2 * parts.length - 1)];
         int i = 0;
@@ -53,8 +48,8 @@ public class Pretty {
             first = false;
             separatedParts[i++] = part;
         }
-        
+
         return concat(separatedParts);
     }
-    
+
 }
