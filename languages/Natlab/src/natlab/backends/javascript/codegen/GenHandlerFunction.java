3e913156297f7de816b9a9e97ed495b1fb02213e
Vincent Foley <vfoley@gmail.com>
2014-03-09 14:49:15 -0400

    Initial code gen

--- languages/Natlab/src/natlab/backends/javascript/codegen/GenHandlerFunction.java
+++ languages/Natlab/src/natlab/backends/javascript/codegen/GenHandlerFunction.java
@@ -1,32 +1,24 @@
 package natlab.backends.javascript.codegen;
 
-import natlab.backends.javascript.jsast.Function;
-import natlab.backends.javascript.jsast.FunctionName;
-import natlab.backends.javascript.jsast.Program;
-import natlab.backends.javascript.jsast.StmtBlock;
-import natlab.backends.javascript.jsast.StmtNull;
-import natlab.backends.javascript.jsast.Variable;
-import natlab.tame.tir.TIRFunction;
-import natlab.tame.tir.TIRStmt;
+import natlab.backends.javascript.jsast.*;
 
 public class GenHandlerFunction {
-    public static void handle(TIRFunction tirFn, Program parent, JSCodeGen gen) {
-        Function fn = new Function();
-        fn.setFunctionName(new FunctionName(tirFn.getName()));
-        
-        for (ast.Name param: tirFn.getInputParams())
-            fn.addParam(new Variable(param.getID()));
-        
-        StmtBlock stmts = new StmtBlock();
-        gen.currentBlock.push(stmts);
-        for (ast.Stmt stmt: tirFn.getStmts()) {
-            ((TIRStmt) stmt).tirAnalyze(gen);
-        }
-        gen.currentBlock.pop();
-        fn.setStmtBlock(stmts);
-        
-        
-        parent.addFunction(fn);
-    }
-    
+//    public static void handle(ast.Function tirFn, Program parent, JSASTGenerator gen) {
+//        Function fn = new Function();
+//        fn.setFunctionName(new FunctionName(tirFn.getName()));
+//        
+//        for (ast.Name param: tirFn.getInputParams())
+//            fn.addParam(new Variable(param.getID()));
+//        
+//        StmtBlock stmts = new StmtBlock();
+//        gen.currentBlock.push(stmts);
+//        for (ast.Stmt stmt: tirFn.getStmts()) {
+//            stmt.analyze(gen);
+//        }
+//        gen.currentBlock.pop();
+//        fn.setStmtBlock(stmts);
+//        
+//        
+//        parent.addFunction(fn);
+//    }
 }
